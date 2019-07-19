de16db3d13da12cba1f3b388036bbd5aee5b071a
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-07-19 07:59:40 +0200

    got a static version of the table being able to render itself

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { groupBy } from 'lodash';
+import ProductCategoryRow from './ProductCategoryRow';
+import ProductRow from './ProductRow';
 
-const productTable = (props) => {};
+const productTable = (props) => {
+  const productsByCategory = groupBy(props.products, product => product.category);
+
+  return (
+    <table>
+
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          Object.keys(productsByCategory).flatMap(category => {
+            const categoryRow = <ProductCategoryRow key={category} category={category} />;
+
+            const productsInCategory = productsByCategory[category].map(({ name, price, stocked }) =>
+              ({ name, price, stocked })
+            );
+            const productsRows = productsInCategory.map(product =>
+              <ProductRow product={product} key={`${category}-${product.name}`} />
+            );
+
+            return [categoryRow, ...productsRows];
+          })
+        }
+      </tbody>
+
+    </table>
+  );
+};
 
 productTable.propTypes = {
-    products: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string,
-        price: PropTypes.string,
-        stocked: PropTypes.bool,
-        category: PropTypes.string
-    }))
+  products: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.string,
+    stocked: PropTypes.bool,
+    category: PropTypes.string
+  }))
 
 };
 
