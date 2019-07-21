1629342cd062a0990aecf846c903b5c9011a080e
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-07-21 06:07:39 +0200

    added props to search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,16 @@ import PropTypes from "prop-types";
 
 const searchBar = props => (
   <div>
-    <input type="text" placeholder="Search..." /> <br />
+    <input type="text" placeholder="Search..." value={props.filterText} /> <br />
     <label>
-      <input type="checkbox" className="no-left-margin" /> Only show products in stock
+      <input type="checkbox" className="no-left-margin" checked={props.showOnlyProductsInStock} /> Only show products in stock
     </label>
   </div>
 );
 
 searchBar.propTypes = {
-
+  filterText: PropTypes.string,
+  showOnlyProductsInStock: PropTypes.bool
 };
 
 export default searchBar;
